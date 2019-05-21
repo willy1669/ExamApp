229e846dc7adb7674c47dd0934909b54de49fa54
willy1669 <williamschinonso11@gmail.com>
2019-05-21 14:28:05 +0100

    create questions

---
+++ app.js
@@ -0,0 +1,5 @@
+var questions = [ 
+    new Question("Who was the first President of United States of America?", ["George Washington", "Thomas Jefferson"], "George Washington"),
+    new Question("What is the answer to the ultimate question of life, The Universe, and Everything?", ["Pi", "42"], "42")
+]
+
