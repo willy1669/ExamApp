0c2a55439b98facc66b354dffae4ddc2be41deea
willy1669 <williamschinonso11@gmail.com>
2019-05-21 14:29:21 +0100

    create an instance of quiz

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ var questions = [
     new Question("What is the answer to the ultimate question of life, The Universe, and Everything?", ["Pi", "42"], "42")
 ]
 
+//create quiz
+var quiz = new quiz(questions);
