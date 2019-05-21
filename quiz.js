7c4aaad74dd7164fcc5222947932a62c27eaddd5
willy1669 <williamschinonso11@gmail.com>
2019-05-21 13:52:56 +0100

    add the quess prototype function

--- quiz.js
+++ quiz.js
@@ -4,3 +4,10 @@ function Quiz (questions) {
     this.currentQuestionIndex = 0;
 }
 
+Quiz.prototype.guess = function (answer) {
+    if (this.getCurrentQuestion().this.CorrectAnswer(answer)){
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+}
+
